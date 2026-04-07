1624322fa755d793c646638204649af93e11369e
NimsaraWickramarathna@ <wenura2003@gmail.com>
2026-04-07 19:48:57 +0530

    Update resourceApi.js
    add resource API module with all CRUD endpoints

--- frontend/src/api/resourceApi.js
+++ frontend/src/api/resourceApi.js
@@ -1,2 +1,20 @@
+import api from './axiosConfig'
 
+export const getResources = (params) =>
+  api.get('/resources', { params })
+
+export const getResourceById = (id) =>
+  api.get(`/resources/${id}`)
+
+export const createResource = (data) =>
+  api.post('/resources', data)
+
+export const updateResource = (id, data) =>
+  api.put(`/resources/${id}`, data)
+
+export const updateResourceStatus = (id, status) =>
+  api.patch(`/resources/${id}/status`, { status })
+
+export const deleteResource = (id) =>
+  api.delete(`/resources/${id}`)
 
